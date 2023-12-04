ca855f2a39e8b0ee841a3b12e21013f2d2a953d2
Harambutt <147921745+Harambutt@users.noreply.github.com>
2023-12-04 14:29:09 +0500

    Add files via upload

---
+++ question 4 (dictionary).py
@@ -0,0 +1,14 @@
+# Define the original dictionary
+sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New York"
+}
+
+# Remove "name" and "salary" keys from the dictionary using del
+del sample_dict["name"]
+del sample_dict["salary"]
+
+# Print the updated dictionary
+print("Updated Dictionary:", sample_dict)
